89213bc6012b65cdfd29b3ffd7cbccc056b78f1c
Krish Jotaniya <krishjotaniya71@gmail.com>
2024-01-25 19:34:57 +0530

    Added folder name in cloudinary

--- backend/utils/Cloudinary.js
+++ backend/utils/Cloudinary.js
@@ -14,7 +14,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null;
         const response = await v2.uploader.upload(localFilePath, {
-            resource_type: "auto"
+            resource_type: "auto",
+            folder: "Library Management System"
         })
         fs.unlinkSync(localFilePath)
         return response;
