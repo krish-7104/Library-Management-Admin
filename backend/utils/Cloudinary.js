25be15f69c5ef5bf237e4b625fa71f048c5844c4
Krish Jotaniya <krishjotaniya71@gmail.com>
2024-04-06 22:33:47 +0530

    Cloudinary Folder removed

--- backend/utils/Cloudinary.js
+++ backend/utils/Cloudinary.js
@@ -15,7 +15,6 @@ const uploadOnCloudinary = async (localFilePath) => {
         if (!localFilePath) return null;
         const response = await v2.uploader.upload(localFilePath, {
             resource_type: "auto",
-            folder: "Library Management System"
         })
         fs.unlinkSync(localFilePath)
         return response;
